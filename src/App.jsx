d3adf72622c21f735487d0764d584ff1dd1b4122
asd <asd.it@gmail.com>
2023-06-25 22:50:39 +0330

    some change

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "./components/Header";
 import "./font.css";
 
-// components
 import Navbar from "./components/Navbar";
 import Search from "./components/Search";
 import Packages from "./components/Packages";
